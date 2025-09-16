57a005cdae78d12783775513276d94e8f7267506
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-16 10:15:59 +0900

    refactor: Align auth system with API specification

--- Desktop/OZ/OZ_Main_AI/users/admin.py
+++ Desktop/OZ/OZ_Main_AI/users/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    pass
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    ordering = ('email',)
+    # UserAdmin의 fieldsets을 커스터마이징하여 username 필드를 제거할 수 있습니다.
+    # 필요 시 아래 주석을 해제하고 수정하세요.
+    # fieldsets = (
+    #     (None, {'fields': ('email', 'password')}),
+    #     ('Personal info', {'fields': ('first_name', 'last_name')}),
+    #     ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    #     ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    # )
--- Desktop/OZ/OZ_Main_AI/users/urls.py
+++ Desktop/OZ/OZ_Main_AI/users/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+from .views import (
+    UserCreateView,
+    UserProfileView,
+    MyTokenObtainPairView,
+    LogoutView,
 )
-from .views import UserCreateView, UserProfileView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('signup/', UserCreateView.as_view(), name='signup'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', UserProfileView.as_view(), name='user_profile'),
 ]
