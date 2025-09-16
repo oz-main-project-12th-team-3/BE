ac8dc2489521e9807e9bc2f7aae24739bb35693d
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-17 01:47:42 +0900

    fix: Add default DB env vars and ensure RUNNING_TESTS=1 in CI

--- config/settings.py
+++ config/settings.py
@@ -96,11 +96,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("DB_NAME"),
-            "USER": os.environ.get("DB_USER"),
-            "PASSWORD": os.environ.get("DB_PASSWORD"),
-            "HOST": os.environ.get("DB_HOST"),
-            "PORT": os.environ.get("DB_PORT"),
+            "NAME": os.environ.get("DB_NAME", "mydatabase"),
+            "USER": os.environ.get("DB_USER", "myuser"),
+            "PASSWORD": os.environ.get("DB_PASSWORD", "mypassword"),
+            "HOST": os.environ.get("DB_HOST", "db"),
+            "PORT": os.environ.get("DB_PORT", "5432"),
         }
     }
 
