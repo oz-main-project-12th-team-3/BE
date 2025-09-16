c7a6d66d260d4514aa22edfdecbc44bd885a22b0
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-16 14:40:19 +0900

    feat: Initial project setup and authentication

--- config/settings.py
+++ config/settings.py
@@ -43,9 +43,12 @@ INSTALLED_APPS = [
     # 3rd party
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 
     # local apps
     'users',
+    'chat',
+    'ai',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,14 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
+
 
 
 # Password validation
