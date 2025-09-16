3a5a0ac0e58a910381309e6e23047f1c7a07c5c3
lee <a50639174@gmail.com>
2025-09-16 00:43:26 -0700

    task 구현

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Task  # Task import 추가
 
-
+# === CustomUser 등록 ===
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     ordering = ('email',)
-    # UserAdmin의 fieldsets을 커스터마이징하여 username 필드를 제거할 수 있습니다.
-    # 필요 시 아래 주석을 해제하고 수정하세요.
+    # fieldsets 커스터마이징 필요 시 아래 주석 해제
     # fieldsets = (
     #     (None, {'fields': ('email', 'password')}),
     #     ('Personal info', {'fields': ('first_name', 'last_name')}),
     #     ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
     #     ('Important dates', {'fields': ('last_login', 'date_joined')}),
     # )
+
+# === Task 등록 ===
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'is_completed', 'due_time', 'created_at', 'updated_at')
+    list_display_links = ('title',)
+    list_filter = ('is_completed', 'due_time', 'created_at')
+    search_fields = ('title', 'description', 'user__email')
+    ordering = ('-created_at',)
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from .views import (
     UserCreateView,
     UserProfileView,
     MyTokenObtainPairView,
     LogoutView,
+    TaskViewSet,  # TaskViewSet import 추가
 )
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='task')  # /tasks/ CRUD 연결
 
 urlpatterns = [
     path('signup/', UserCreateView.as_view(), name='signup'),
@@ -13,4 +18,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', UserProfileView.as_view(), name='user_profile'),
+
+    path('', include(router.urls)),  # TaskViewSet 포함
 ]
