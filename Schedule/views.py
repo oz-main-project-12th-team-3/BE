75514b6879061693ae0a330706664773ac21d8ec
lee <a50639174@gmail.com>
2025-09-19 04:10:00 -0700

    스케줄 앱 전체 테스트 완료 및 설정/URL 수정 반영

--- Schedule/views.py
+++ Schedule/views.py
@@ -1,5 +1,4 @@
 from rest_framework import permissions, viewsets
-
 from .models import Schedule
 from .serializers import ScheduleSerializer
 
@@ -15,7 +14,9 @@ class ScheduleViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
+        """로그인한 사용자의 일정만 반환"""
         return Schedule.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
+        """일정 생성 시 user 필드를 현재 사용자로 자동 지정"""
         serializer.save(user=self.request.user)
