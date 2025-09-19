c0f563918a449942d81c5eae9e0a7b566574db64
lee <a50639174@gmail.com>
2025-09-19 03:07:18 -0700

    테스트 코드: import 정렬 및 랜덤 비밀번호 적용

--- Schedule/tests.py
+++ Schedule/tests.py
@@ -1,6 +1,8 @@
 import secrets
+
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 
