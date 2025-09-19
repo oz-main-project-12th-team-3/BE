9afb823f072c634ed0e3404f4d31e8298ad16262
lee <a50639174@gmail.com>
2025-09-19 04:24:50 -0700

    Fix REST_FRAMEWORK settings and apply ruff formatting

--- config/settings.py
+++ config/settings.py
@@ -169,7 +169,11 @@ CSRF_COOKIE_SECURE = SECURE_COOKIE
 # 패스워드 검증
 # -----------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {
+        "NAME": (
+            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+        )
+    },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
