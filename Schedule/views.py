bec397fa4984c91e1c67ce5f9df52e637ee8bf38
lee <a50639174@gmail.com>
2025-09-18 19:57:20 -0700

    Add Schedule app and initial migrations

---
+++ Schedule/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets, permissions
+from .models import Schedule
+from .serializers import ScheduleSerializer
+
+class ScheduleViewSet(viewsets.ModelViewSet):
+    """
+    Schedule CRUD API
+    - 로그인한 사용자만 접근 가능
+    - 생성 시 현재 사용자 자동 지정
+    """
+    serializer_class = ScheduleSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Schedule.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line utility for administrative Schedule."""
 
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative Schedule."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
