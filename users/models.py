613a3375ba59465bed87ed8f9b0dde12868e0138
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-23 15:59:41 +0900

    SCRUM-10: Chore: Remove long comment line causing lint error

--- users/models.py
+++ users/models.py
@@ -110,4 +110,4 @@ class UserProfile(models.Model):
 # @receiver(post_save, sender=User)
 # def create_user_profile(sender, instance, created, **kwargs):
 #     if created and not hasattr(instance, "user_profile"):
-#         UserProfile.objects.create(user=instance, nickname=instance.email.split('@')[0])
+
