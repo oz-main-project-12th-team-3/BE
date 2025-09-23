e12dab4b29bf28a503c3c2dc74697d2fbb4f1c35
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-23 15:57:09 +0900

    SCRUM-10: Chore: Fix ruff formatting in users/views

--- users/views.py
+++ users/views.py
@@ -81,7 +81,7 @@ class UserLoginView(APIView):
                 expires=datetime.now(timezone.utc)
                 + settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"],
             )
-                        # Also set access_token for convenience
+            # Also set access_token for convenience
             response.set_cookie(
                 key="access_token",
                 value=access_token,
