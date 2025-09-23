ee896cedf9e7dc14c6ac160e43348f309cf1b0c0
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-23 16:10:08 +0900

    SCRUM-10: Fix: Add project root to sys.path for CI tests

--- conftest.py
+++ conftest.py
@@ -1,9 +1,13 @@
 import os
+import sys
+from pathlib import Path
 
 import django
 
-pytest_plugins = ["pytest_django"]
+# Add the project root to the Python path
+sys.path.insert(0, str(Path(__file__).resolve().parent))
 
+pytest_plugins = ["pytest_django"]
 
 def pytest_configure():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
