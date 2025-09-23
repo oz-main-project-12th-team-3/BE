def314d0b1ea112778ebc9abf151795d2a092e57
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-23 16:17:26 +0900

    SCRUM-10: Fix: Force add project root to sys.path in conftest

--- conftest.py
+++ conftest.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 import django
 
-# Add the project root to the Python path
-sys.path.insert(0, str(Path(__file__).resolve().parent))
+# Add the project root to the Python path to ensure all apps are discoverable.
+ROOT_DIR = Path(__file__).resolve().parent
+sys.path.insert(0, str(ROOT_DIR))
 
 pytest_plugins = ["pytest_django"]
-
 
 def pytest_configure():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
