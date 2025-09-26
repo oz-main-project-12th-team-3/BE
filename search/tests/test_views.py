c6c6ac9cd3ce9ca2a5038f6d54dd220debad5a2b
lee <a50639174@gmail.com>
2025-09-25 18:53:42 -0700

    코드수정

--- search/tests/test_views.py
+++ search/tests/test_views.py
@@ -23,7 +23,7 @@ class TestSearchLogViews:
         return client
 
     def test_create_search_log_authenticated(self, auth_client, user):
-        url = reverse("search-log-list-create")
+        url = reverse("search:search-log-list-create")  # 수정됨
         data = {"keyword": "Django", "search_type": "tutorial", "result_count": 5}
         response = auth_client.post(url, data, format="json")
         assert response.status_code == 201
@@ -32,20 +32,20 @@ class TestSearchLogViews:
 
     def test_create_search_log_unauthenticated(self):
         client = APIClient()
-        url = reverse("search-log-list-create")
+        url = reverse("search:search-log-list-create")  # 수정됨
         data = {"keyword": "Django", "search_type": "tutorial", "result_count": 5}
         response = client.post(url, data, format="json")
         assert response.status_code == 403
 
     def test_list_search_logs_authenticated(self, auth_client, user):
         SearchLog.objects.create(user=user, keyword="DRF", result_count=10)
-        url = reverse("search-log-list-create")
+        url = reverse("search:search-log-list-create")  # 수정됨
         response = auth_client.get(url)
         assert response.status_code == 200
         assert any(log["keyword"] == "DRF" for log in response.data)
 
     def test_list_search_logs_unauthenticated(self):
         client = APIClient()
-        url = reverse("search-log-list-create")
+        url = reverse("search:search-log-list-create")  # 수정됨
         response = client.get(url)
         assert response.status_code == 403
