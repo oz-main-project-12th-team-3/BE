1ce36ba860a35223f799164481a56c278714d6c6
209512 <209512mon@gmail.com>
2025-09-26 15:49:27 +0900

    feat: 임시 토큰, 비밀번호 변경
    회원가입 시 2fa 활성 사용자는 로그인 시 임시 토큰 발급, 바로 2fa 설정 가능
    비밀번호 변겅 시 입력은 새 비밀번호만으로 변경

--- users/views/user_views.py
+++ users/views/user_views.py
@@ -69,17 +69,13 @@ class PasswordChangeView(APIView):
         new_password = serializer.validated_data["new_password"]
 
         try:
-            # change_user_password는 이제 토큰 재발급 하지 않음 (None 반환)
-            _ = user_service.change_user_password(user, current_password, new_password)
-
-            # 기존 토큰 블랙리스트 처리로 인해 모든 토큰 무효화됨 -> 자동 로그아웃 상태
+            _ = user_service.change_user_password(user, new_password)
             response = Response(
                 {
                     "detail": "비밀번호가 성공적으로 변경되었습니다. 다시 로그인해 주세요."
                 },
                 status=status.HTTP_200_OK,
             )
-            # 클라이언트가 기존 쿠키를 지울 수 있도록 쿠키 삭제
             response.delete_cookie("access_token")
             response.delete_cookie("refresh_token")
             return response
