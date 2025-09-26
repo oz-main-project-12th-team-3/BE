4f99e13f83917216c0dd10b62a02040c7a8a3dcf
lee <a50639174@gmail.com>
2025-09-25 18:35:12 -0700

    오류

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,7 @@
-# search/urls.py
 from django.urls import path
+from .views import SearchLogListCreateView
 
-from search.views import SearchLogListCreateView
+app_name = "search"  # 네임스페이스 선언
 
 urlpatterns = [
     path("logs/", SearchLogListCreateView.as_view(), name="search-log-list-create"),
