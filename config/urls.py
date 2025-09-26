9f566b8ea18c7cdfb5d187822c02373f68c67eba
lee <a50639174@gmail.com>
2025-09-25 18:40:54 -0700

    포멧

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
 from django.contrib import admin
 from django.urls import include, path
 
@@ -26,6 +25,3 @@ urlpatterns = [
     path("api/", include("schedule.urls")),
     path("search/", include(("search.urls", "search"), namespace="search")),
 ]
-
-
-
