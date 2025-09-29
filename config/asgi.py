b5f4170eb3b18366798315ee726f41b6690d2a11
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-30 02:50:51 +0900

    fix(asgi): Correctly order imports and restore JwtAuthMiddleware

--- config/asgi.py
+++ config/asgi.py
@@ -2,7 +2,6 @@ import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
 from chat.middleware import JwtAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
