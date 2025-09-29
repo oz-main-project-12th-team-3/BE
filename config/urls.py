fe3809059ecb1b46f7050df3607b84baf9b8bc14
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-29 16:30:45 +0900

    feat: Enable Swagger UI with drf-spectacular

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,6 +11,9 @@ urlpatterns = [
     path("api/", include("search.urls")),
     path("api/", include("notifications.urls")),
     path("api/payments/", include("payments.urls")),
+    # drf-spectacular URLS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 # if not settings.IS_TEST_ENV:
