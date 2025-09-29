3aca02b3797060dfc760c7a235f4a3bf7ea21ff7
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-29 15:13:41 +0900

    fix(asgi): Resolve AppRegistryNotReady error on startup

--- config/asgi.py
+++ config/asgi.py
@@ -1,20 +1,17 @@
 import os
-
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import chat.routing
-
-# Set the DJANGO_SETTINGS_MODULE environment variable.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
 
-# Initialize the Django application first. This runs django.setup().
-django_asgi_app = get_asgi_application()
+import chat.routing
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
+        "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
     }
 )
