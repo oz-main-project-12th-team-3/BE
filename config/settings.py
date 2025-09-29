4958530c032fa5c9a2dc419147d72ae8630946fb
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-29 16:30:45 +0900

    feat: Enable Swagger UI with drf-spectacular

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+    "drf_spectacular",
     # local apps
     "users",
     "chat",
@@ -194,6 +195,7 @@ else:
             "rest_framework_simplejwt.authentication.JWTAuthentication",
         ),
         "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+        "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     }
 
 
@@ -319,3 +321,11 @@ EMAIL_HOST_PASSWORD = os.environ.get(
     "EMAIL_HOST_PASSWORD"
 )  # 비밀번호(또는 앱 비밀번호)
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  # 발신 이메일 주소 기본값
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'OZ-Digital-Human API',
+    'DESCRIPTION': 'API documentation for the OZ Digital Human project.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
