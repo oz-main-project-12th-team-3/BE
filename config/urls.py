e9cf26d2751b3ef55dcbe55839188050515ab0f1
209512 <209512mon@gmail.com>
2025-09-30 09:12:56 +0900

    fix: django-two-factor-auth, 도커, nginx
    develop 브랜치에서 분기.
    
    django-two-factor-auth 페이지 사용가능 해보임.
    - settings.py 에 2fa 일부 추가 밑 주석 처리 해제
    - 커스텀 로그인 페이지에서 2fa 기능으로 넘어갈 시 2fa 페이지 사용가능
    - 다만 2FA 페이지를 실제로 제대로 사용하려면 CSRF 토큰, 세션쿠키 등을 다루는 프론트엔드(웹 UI 혹은 React, Vue 같은 SPA) 필요
    - 프론트엔드가 이 토큰과 세션을 관리하고 2FA 입력폼을 제공, post 요청을 보내는 역할
    - 프론트엔드가 없으면 curl 명령어로도 API 호출은 가능하지만, 쿠키와 CSRF 토큰을 정확하게 관리하기 매우 어렵기 때문에 테스트와 실제 사용에 한계
    -지금의 설정 상태에서는 프론트엔드가 있으면 2FA 화면이 정상 작동할 것으로 예상.
    
    nginx/default.conf 웹소켓 사용 시 필요하다는 부분 추가. 확인 필요.
    
     기존 도커파일에서 주석처리해뒀던 two_factor 패치 스크립트 재활용.
     - 하지만 빌드시 용량 과부하 발생
     - 도커파일 최적화, 패치 스크립트 최적화
     -- two_factor urls.py의 url 패턴이 현재 장고에서 지원하지 않는 버전이기에 사용 시에 반드시 수정 필요. 수동 변경도 가능. 하지만 불편.
     -도커파일 최적화 수정사항에 따른 docker-compose.yml 도 변경. 작동 확인 필요
    
    python manage.py show url 입력 시 현재 url과, django-two-factor-auth url 확인 가능.
    /api/account/login/           -> two_factor:login
    /api/account/two_factor/      -> two_factor:profile
    /api/account/two_factor/setup/ -> two_factor:setup
    - 앞부분 "api/"는 config/urls.py에서 변경 가능. "account/two_factor/setup/" 뒷부분은 커스텀 필요시 수정 가능해보임.

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/', include('two_factor.urls')),
     path("api/", include("users.urls")),
     path("api/", include("chat.urls")),
     path("api/", include("schedule.urls")),
