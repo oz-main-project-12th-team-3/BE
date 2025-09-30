4c4f4073f0f13663d409a61cc3b4ba3aceb1a46b
209512 <209512mon@gmail.com>
2025-09-30 14:26:08 +0900

    fix: 러프 체크 방지 주석 추가

--- config/asgi.py
+++ config/asgi.py
@@ -6,16 +6,17 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 # 2. Call django.setup() to initialize the app registry
 #    This must happen BEFORE any application-dependent imports.
 import django
+
 django.setup()
 
 # 3. Import all application-dependent code (like middleware, routing)
 #    These imports are now safe because Django is set up.
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa
+from django.core.asgi import get_asgi_application  # noqa
 
 # Your custom imports are now safe
-from chat.middleware import JwtAuthMiddleware
-import chat.routing
+from chat.middleware import JwtAuthMiddleware  # noqa
+import chat.routing  # noqa
 
 
 application = ProtocolTypeRouter(
