4c1ea35c5db124123c4ecd1d635a8e7420c9e24a
BE12-OZ <dhwmzheld6470@gmail.com>
2025-09-30 20:35:34 +0900

    fix: Add drf-spectacular to INSTALLED_APPS (SCRUM-25)

--- config/settings.py
+++ config/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+    "drf_spectacular",
     # local apps
     "users",
     "chat",
