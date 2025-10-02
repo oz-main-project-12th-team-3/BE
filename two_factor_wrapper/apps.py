ba97c2600e1969105e0c79699d3b383caacbd49f
209512 <209512mon@gmail.com>
2025-10-02 10:57:14 +0900

    style: ruff

--- two_factor_wrapper/apps.py
+++ two_factor_wrapper/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
+
 class TwoFactorWrapperConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'two_factor_wrapper'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "two_factor_wrapper"
     # 이 부분은 필수 아님: verbose_name = 'Two Factor URL Wrapper'
