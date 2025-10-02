e69020adc8089f6659a2d2d00e08ac83b0c4905a
BE12-OZ <dhwmzheld6470@gmail.com>
2025-10-02 14:01:22 +0900

    SCRUM-12: 프론트엔드/백엔드 통합 배포 설정

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'dist')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -266,6 +266,10 @@ USE_TZ = True
 # -----------------------------
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'dist'),
+]
 
 
 # -----------------------------
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.views.generic import TemplateView
+from two_factor.urls import urlpatterns as two_factor_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("two_factor.urls")),
+    path("api/", include(two_factor_urls)),
     path("api/", include("users.urls")),
     path("api/", include("chat.urls")),
     path("api/", include("schedule.urls")),
@@ -18,6 +20,8 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    # Catch-all for frontend
+    re_path(r"^.*$", TemplateView.as_view(template_name="index.html")),
 ]
 
 # if not settings.IS_TEST_ENV:
