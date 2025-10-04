082ae4b01b2069fdc75cc275a5d043b5ec64c1b3
BE12-OZ <dhwmzheld6470@gmail.com>
2025-10-04 18:16:35 +0900

    SCRUM-15: refactor(deploy): Consolidate deploy scripts into start.sh

--- .platform/hooks/postdeploy/01_migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ">>> 01_migrate.sh is executing!"
-
-echo ">>> Running database migrations..."
-python3 manage.py migrate --noinput
-
-echo ">>> Running collectstatic..."
-python3 manage.py collectstatic --noinput
