cc82bbf134cc3563bcebac47bbe86d1bbd6fca59
BE12-OZ <dhwmzheld6470@gmail.com>
2025-10-04 17:14:36 +0900

    SCRUM-12: fix(deploy): Run collectstatic and fix nginx static path

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo ">>> 01_migrate.sh is executing!"
+
+echo ">>> Running database migrations..."
+python manage.py migrate --noinput
+
+echo ">>> Running collectstatic..."
+python manage.py collectstatic --noinput
