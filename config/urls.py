c8c6cf3c7a58d573f654d894a590e0832aa54d02
BE12-OZ <dhwmzheld6470@gmail.com>
2025-10-11 18:46:37 +0900

    chore: CI/CD 파이프라인 린트 오류 수정
    urls.py에서 사용하지 않는 import(re_path, TemplateView)를 제거하여 `ruff` 린트 오류를 해결했습니다.
    
    SCRUM-15

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import include, path, re_path
-from django.views.generic import TemplateView
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
