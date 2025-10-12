c0edfb7364219f71103bab5bbae7bc92f8aa1570
BE12-OZ <dhwmzheld6470@gmail.com>
2025-10-13 04:49:04 +0900

    SCRUM-22: Temporarily disable Djoser SEND_ACTIVATION_EMAIL for 504 signup debug

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ DJOSER = {
     "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
     "USERNAME_RESET_CONFIRM_URL": "username/reset/confirm/{uid}/{token}",
     "ACTIVATION_URL": "activate/{uid}/{token}",
-    "SEND_ACTIVATION_EMAIL": True,
+    "SEND_ACTIVATION_EMAIL": False,
     "SOCIAL_AUTH_TOKEN_STRATEGY": "djoser.social.token.jwt.TokenStrategy",
     "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS": os.environ.get(
         "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS", ""
