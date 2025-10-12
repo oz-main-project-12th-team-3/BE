baaec1ee7f43af4019e8d864be6ea4c5cc99f403
BE12-OZ <dhwmzheld6470@gmail.com>
2025-10-12 17:23:49 +0900

    SCRUM-XX: [Backend] Enhance login 401 error message format

--- users/views/auth_views.py
+++ users/views/auth_views.py
@@ -230,7 +230,10 @@ class UserLoginView(APIView):
                 "temporary_refresh_token": None,
                 "profile_image_url": None,
             }
-            return Response(response_data, status=status.HTTP_401_UNAUTHORIZED)
+            # LoginResponseSerializer를 사용하여 일관된 출력 형식 보장
+            error_serializer = LoginResponseSerializer(data=response_data)
+            error_serializer.is_valid(raise_exception=True) # 오류 응답 구조 유효성 검사
+            return Response(error_serializer.validated_data, status=status.HTTP_401_UNAUTHORIZED)
 
         login(request, user)  # Django 세션 로그인
 
